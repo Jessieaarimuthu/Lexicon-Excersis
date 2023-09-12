9c2d65d5c5cf4c30c3896ef9c727bc15c9441637
Jessieaarimuthu <144336192+Jessieaarimuthu@users.noreply.github.com>
2023-09-12 17:03:56 +0200

    Exercises 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Employee
+{
+
+    static void Main(string[] args)
+    {
+        string Name;
+        int Salary;
+        Console.WriteLine("Enter Employee Name: ");
+        Name = Console.ReadLine();
+
+        Console.WriteLine("Enter Employee Salary: ");
+        Salary = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Employee Name: " + Name);
+        Console.WriteLine("Employee Salary: " + Salary);
+    }
+}
