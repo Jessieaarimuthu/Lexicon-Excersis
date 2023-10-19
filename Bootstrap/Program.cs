997f6e83801d5c6fd750bb7273f03cc8fed08439
Jessieaarimuthu <144336192+Jessieaarimuthu@users.noreply.github.com>
2023-10-19 11:36:33 +0200

    Add files via upload

---
+++ Bootstrap/Program.cs
@@ -0,0 +1,11 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => {
+    var html = System.IO.File.ReadAllText("/Users/jessiedaniel/Projects/HTML &CSS/HTML &CSS/Index.html");
+    return html;
+});
+
+
+app.Run();
+
