80c422cb6f0a6b09f75639d6c83d529c7d3c4423
Jessieaarimuthu <144336192+Jessieaarimuthu@users.noreply.github.com>
2023-10-23 13:55:59 +0200

    Add files via upload

---
+++ Garage/Garage/GarageHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Garage
+{
+    class GarageHandler
+    {
+        private Garage<Vehicle> theGarage;
+
+        public void CreateGarage(int capacity)
+        {
+            theGarage = new Garage<Vehicle>(capacity);
+        }
+    
+        public bool Initialized() => !(theGarage == null);
+
+        public bool AddVehicle(Vehicle theVehicle)
+        {
+            if (theGarage.Count == theGarage.Capacity) return false;
+            else
+            {
+                theGarage.AddVehicle(theVehicle);
+                return true;
+            }
+        }
+
+        public bool RemoveVehicle(string REGNO)
+        {
+            return theGarage.RemoveVehicle(REGNO);
+        }
+
+        public Vehicle FindVehicle(string REGNO)
+        {
+            return theGarage.FindVehicle(REGNO);
+        }
+
+        public IEnumerable<Vehicle> ListVehicles()
+        { 
+            return theGarage;
+        }
+
+        public IEnumerable<Vehicle> GetCharacterizedVehicles(int wheels, string color)
+        {
+            return theGarage.Where(v =>
+               (wheels <= 0 || v.Wheels == wheels) &&
+               (color != null || v.Color == color)
+            );
+        }
+      
+    }
+}
---
+++ Garage/Garage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Garage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GarageApplication theApplication = new GarageApplication();
+            theApplication.Run();
+        }
+        
+    }
+}
+
+
+
